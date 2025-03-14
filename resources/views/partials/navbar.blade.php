698d24864540ec48750be3ec5b1a167223cb7eb3
J-amex <ar.james.contact@gmail.com>
2025-03-14 10:34:02 +0800

    nav fix fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,30 +3,30 @@
          style="background-color: #110C3D; min-height: 700px; max-height: 800px;">
         <!-- Navigation Links -->
         <nav class="nav flex-column">
-    <a href="{{ route('admin.dashboard') }}" 
-       class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.dashboard') ? 'active-link' : '' }}">
-        <img src="{{ asset('images/icn dashboard.png') }}" alt="Dashboard" width="30" height="30" class="me-2">    
-        Dashboard
-    </a>
+            <a href="{{ route('admin.dashboard') }}" 
+            class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.dashboard') ? 'active-link' : '' }}">
+                <img src="{{ asset('images/icn dashboard.png') }}" alt="Dashboard" width="30" height="30" class="me-2">    
+                Dashboard
+            </a>
 
-    <a href="{{ route('admin.createExam') }}" 
-       class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.createExam') ? 'active-link' : '' }}">
-        <img src="{{ asset('images/icn createExam.png') }}" alt="Create Exam" width="30" height="30" class="me-2"> 
-        Create Exam
-    </a>
+            <a href="{{ route('admin.createExam') }}" 
+            class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.createExam') ? 'active-link' : '' }}">
+                <img src="{{ asset('images/icn createExam.png') }}" alt="Create Exam" width="30" height="30" class="me-2"> 
+                Create Exam
+            </a>
 
-    <a href="{{ route('admin.dashboard') }}" 
-       class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.dashboard') ? 'active-link' : '' }}">
-        <img src="{{ asset('images/icn agent.png') }}" alt="Agents" width="30" height="30" class="me-2">  
-        Agents
-    </a>
+            <a href="{{ route('admin.agents') }}" 
+            class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.agents') ? 'active-link' : '' }}">
+                <img src="{{ asset('images/icn agent.png') }}" alt="Agents" width="30" height="30" class="me-2">  
+                Agents
+            </a>
 
-    <a href="{{ route('admin.dashboard') }}" 
-       class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.dashboard') ? 'active-link' : '' }}">
-        <img src="{{ asset('images/icn examArchive.png') }}" alt="Exam Archive" width="30" height="30" class="me-2">  
-        Exam Archive
-    </a>
-</nav>
+            <a href="{{ route('admin.examArchive') }}" 
+            class="nav-link text-white fw-bold p-3 {{ request()->routeIs('admin.examArchive') ? 'active-link' : '' }}">
+                <img src="{{ asset('images/icn examArchive.png') }}" alt="Exam Archive" width="30" height="30" class="me-2">  
+                Exam Archive
+            </a>
+        </nav>
 
 
         <div class="mt-auto">
