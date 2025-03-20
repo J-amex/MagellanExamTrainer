fdb003cd9a27856f740af61f5603524e082736bd
J-amex <ar.james.contact@gmail.com>
2025-03-20 14:05:33 +0800

    exam_archive

---
+++ database/migrations/2025_03_20_060314_create_exam_archive_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_archive', function (Blueprint $table) {
+            $table->id('exam_id')->unique();; 
+            $table->string('exam_title');
+            $table->string('examiner');
+            $table->string('time_limit');
+            $table->string('times_taken');
+            $table->string('failed_percentage');
+            $table->string('ratings_avg');
+            $table->string('survery_results');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_archive');
+    }
+};
