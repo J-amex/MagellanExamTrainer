ca8d1b9694b126d2a0ff8205feeca23e2987cb3e
J-amex <ar.james.contact@gmail.com>
2025-03-20 13:53:52 +0800

    exams table

---
+++ database/migrations/2025_03_20_054905_create_exams_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams_table', function (Blueprint $table) {
+            $table->id('exam_id'); 
+            $table->string('exam_title')->unique();
+            $table->string('date');
+            $table->string('time_limit');
+            $table->string('file_path');
+            $table->string('question');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams_table');
+    }
+};
