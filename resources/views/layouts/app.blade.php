6ba61b73dc56959372e9e95a81b5f8f3a91e1324
J-amex <ar.james.contact@gmail.com>
2025-03-27 06:24:46 +0800

    added data tables cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
     <!-- ✅ Bootstrap via CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- datatable -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+
    
 
 <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
@@ -33,6 +36,13 @@
     <!-- charts -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+
+    <!-- ✅ jQuery (Required for DataTables) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- ✅ DataTables JS -->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 
     
     @yield('scripts')
